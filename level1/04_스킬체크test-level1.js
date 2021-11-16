e28f028406c8d8db2a5ca974dacaf1a954f75777
sunysty <seokjiseon@gmail.com>
2021-11-16 10:34:49 +0900

    📝 11/14

--- level1/04_스킬체크test-level1.js
+++ level1/04_스킬체크test-level1.js
@@ -1,6 +1,6 @@
 /* 11/14(금) 
 
-문제 설명
+문제1 설명
 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
 
 제한 조건
@@ -22,4 +22,34 @@ function solution(s) {
     return answer;
 }
 
-console.log(solution(-1234))
+console.log(solution(-1234));
+
+
+//================================
+
+/*
+문제2 설명
+함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
+다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+제한 조건
+x는 -10000000 이상, 10000000 이하인 정수입니다.
+n은 1000 이하인 자연수입니다.
+입출력 예
+x	n	answer
+2	5	[2,4,6,8,10]
+4	3	[4,8,12]
+-4	2	[-4, -8]
+
+*/
+
+
+function solution(x, n) {
+    var answer = [];
+
+    for(let i=1; i<=n; i++){
+        answer.push(i*n);
+    }
+    return answer;
+}
+
