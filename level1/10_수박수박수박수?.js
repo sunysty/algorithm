3a74e5076e61278bfe12aaf9ac96b7649a46fe59
sunysty <seokjiseon@gmail.com>
2021-11-24 10:21:24 +0900

    📝 11/23

--- level1/10_수박수박수박수?.js
+++ level1/10_수박수박수박수?.js
@@ -35,7 +35,7 @@ const solution = n => {
     let answer = ''
     
     for(let i = 0; i < n; i++) {
-        i%2===0 ? answer += "수" :answer += "박";
+        i%2 == 0 ? answer += "수" : answer += "박";
     }
     
     return answer
